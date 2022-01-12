ac37f01a6e59d6a447133767fdfe03bcbda3f200
Hammami <ahmed.hammami.1@esprit.tn>
2022-01-12 17:29:58 +0100

    commit on main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,33 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Test</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
+    <body class="bg-grey-300">
+        <nav class="p-6 bg-white flex justify-between">
+            <ul class="flex items-center">
+                <li>
+                     <a href="" class="p-3">loula</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">thenya</a>
+                </li>
+                <li> 
+                    <a href="" class="p-3">theltha</a>
+                </li>
+            </ul>
+            <ul class="flex items-center">
+                <li>
+                     <a href="" class="p-3">loula</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">thenya</a>
+                </li>
+                <li> 
+                    <a href="" class="p-3">theltha</a>
+                </li>
+            </ul>
+        </nav>
         @yield('content')
     </body>
 </html>
